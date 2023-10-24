1806800d1e4fdec32bb44cca81451e0a1b43aeb1
otavioporto <RM550715@fiap.com.br>
2023-10-23 21:15:47 -0300

    Adicionando Conteudo da página calca

---
+++ src/app/produtos/estaticos/itens/calca/page.jsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export default function Calca({params}) {
+    return (
+      <div>
+          <h1></h1>
+          <div>
+              <p>Valor: {params.calca}</p>
+              <p> <Link href="/">Voltar</Link></p>
+              <figure>
+                <Image src=""
+                    width={500}
+                    height={500}
+                    alt="Boné"
+                    />
+                  <figcaptio></figcaptio>
+              </figure>
+              
+          </div>
+      </div>
+    ); 
+  }
